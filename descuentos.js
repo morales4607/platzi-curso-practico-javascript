078518359f5ab279a2cb95ce58f58f3c08ab9348
Daniel Morales <morales4607@gmail.com>
2021-08-10 06:58:13 -0500

    Ejercicio descuento con cupones resuelto

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,13 @@
 /* const precioOriginal = 100;
 const descuento = 15;
  */
+
+const coupons = [
+    "MetalCore",
+    "JavaScript",
+    "Metallica"
+]
+
 function calculaPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -11,10 +18,27 @@ function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
 
-    const InputDiscount = document.getElementById("InputDiscount");
-    const discountValue = InputDiscount.value;
+    const InputCoupon = document.getElementById("InputCoupon");
+    const couponValue = InputCoupon.value;
 
-    const precioConDescuento = calculaPrecioConDescuento(priceValue, discountValue);
+    /* const InputDiscount = document.getElementById("InputDiscount");
+    const discountValue = InputDiscount.value; */
+
+    let descuento;
+
+    switch(couponValue) {
+        case "MetalCore":
+            descuento  = 15;
+            break;
+            case "JavaScript":
+                descuento = 30;
+                break;
+                case "Metallica":
+                    descuento = 25;
+                    break;
+    }
+
+    const precioConDescuento = calculaPrecioConDescuento(priceValue, descuento);
 
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
