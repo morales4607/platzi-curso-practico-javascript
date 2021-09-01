e8819cbc458b44baadfe25abeeff6e8ecdd83420
Daniel Morales <morales4607@gmail.com>
2021-09-01 07:44:08 -0500

    Calculando la mediana de salarios

---
+++ salarios.js
@@ -0,0 +1,61 @@
+const colombia = [];
+colombia.push({
+    name: "Juan",
+    salary: 500,
+});
+colombia.push({
+    name: "Felipe",
+    salary: 1500,
+});
+colombia.push({
+    name: "Mariana",
+    salary: 1800,
+});
+colombia.push({
+    name: "Manuel",
+    salary: 2500,
+});
+colombia.push({
+    name: "Ana",
+    salary: 2000,
+});
+colombia.push({
+    name: "Daniel",
+    salary: 1200,
+});
+colombia.push({
+    name: "Maria",
+    salary: 2200,
+});
+colombia.push({
+    name: "Luisa",
+    salary: 2100,
+});
+colombia.push({
+    name: "Esteban",
+    salary: 3000,
+});
+colombia.push({
+    name: "Yuliana",
+    salary: 1700,
+});
+colombia.push({
+    name: "Isabella",
+    salary: 1300,
+});
+colombia.push({
+    name: "Milena",
+    salary: 2400,
+});
+colombia.push({
+    name: "Alejandro",
+    salary: 1600,
+});
+colombia.push({
+    name: "Mario",
+    salary: 1900,
+});
+colombia.push({
+    name: "Carlos",
+    salary: 1600,
+});
